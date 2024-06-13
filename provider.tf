635be2aac62ad09ff56da281317d70dfd948a6a4
ReetishK <reetish244@gmail.com>
2024-06-13 11:23:46 +0000

    providerupdate

--- provider.tf
+++ provider.tf
@@ -19,5 +19,24 @@ provider "azurerm" {
   features {}
   #
   # delete_vm_but_do_not_delete_storage
-
+  subscription_id = var.subscription_id 
+  client_id =  var.client_id
+  client_secret =  var.client_secret
+  tenant_id =  var.tenant_id
 }
+
+variable "client_id" {
+  type = string 
+}
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
